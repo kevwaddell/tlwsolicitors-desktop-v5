4c5d6d56eb4ebe6ade2a5efa01b38a7e70a1c9f6
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-08-17 17:06:30 +0100

    Tidy up unused css and js.

--- _/js/functions.js
+++ _/js/functions.js
@@ -46,11 +46,6 @@
 		
 	$(document).ready(function (){
 	
-	var service_select = $('select#service-select');
-	var service_area_select = $('select.service-area-select');
-	var child_service_area_select = $('select.child-service-area-select');
-	var start_enquiry_btn = $('a#start-enquiry-btn');
-	
 	var section_targets = $('a.section-target');
 	
 	if (section_targets.length > 0) {
@@ -75,151 +70,6 @@
 		width: '100%'
 	});
 	
-	$('#services-dd').selectpicker({
-      size: 6,
-      showContent: false
-	  });
-
-	
-	$('#services-dd').on('changed.bs.select', function (event, clickedIndex) {
-		
-		var url = $('#services-dd').find('option').eq(clickedIndex).val();
-		
-		if (url.indexOf("#") === 0) {
-
-		var scrollTarget = $(url).offset().top - 40;
-		
-			if ($('body').hasClass('admin-bar')) {
-			scrollTarget = scrollTarget + 32;	
-			}
-		
-		$('html, body').animate({ scrollTop: scrollTarget }, 500);	
-		$(this).selectpicker('refresh');
-		} else {
-		window.location = url;	
-		}
-		
-	});
-	  
-	  $(service_area_select).selectpicker('hide');
-	  $(child_service_area_select).selectpicker('hide');
-	  
-	 $(service_select).on("change", function(){
-		var selected_id = "#"+$(this).find('option:selected').html().replace(/\s+/g, '-').replace(/&nbsp;/g, '-').toLowerCase()+"-select";
-		var val = $(this).val();
-		
-		$(service_area_select).selectpicker('hide');
-		
-		if ($('.submit-btn').hasClass('hidden')) {
-			$('.submit-btn').removeClass('hidden').addClass('animated fadeIn');
-		} 
-
-		if ( $(this).attr("name") === "main-service-area" && val !== 0) {
-			$(start_enquiry_btn).attr('href', val);
-		}
-		
-		if ( $(this).attr("name") === "service" && val !== 0) {
-			$(start_enquiry_btn).attr('href', val);
-		}
-
-		
-		$(selected_id).selectpicker('show');
-		
-	 }); 
-	 
-	 $(service_area_select).on("change", function(){
-		var selected_id = "#"+$(this).find('option:selected').html().replace(/\s+/g, '-').replace(/&nbsp;/g, '-').toLowerCase()+"-select";
-		var val = $(this).val();
-		
-		 $(child_service_area_select).selectpicker('hide');
-		
-		if ($('.submit-btn').hasClass('hidden')) {
-			$('.submit-btn').removeClass('hidden').addClass('animated fadeIn');
-		} 
-		
-		if ( $(this).attr("name") === "main-service-area" && val !== 0) {
-			$(start_enquiry_btn).attr('href', val);
-		}
-		
-		$(selected_id).selectpicker('show');
-		
-	 }); 
-	 
-	 $(child_service_area_select).on("change", function(){
-		var val = $(this).val();
-		
-		if ($('.submit-btn').hasClass('hidden')) {
-			$('.submit-btn').removeClass('hidden').addClass('animated fadeIn');
-		} 
-		
-		if ( $(this).attr("name") === "child-service-area" && val !== 0) {
-			$(start_enquiry_btn).attr('href', val);
-		}
-		
-	 }); 
-	 
-	 //How it Works link
-	 
-	 $('div.how-it-works-link').on(event_type,'a', function(){
-		
-		var how_it_works_id = $(this).attr('href');
-	
-		if ( $(how_it_works_id).hasClass('hidden') ) {
-			
-			$(how_it_works_id).removeClass('hidden').addClass('animated fadeIn');
-			$('.tlw-wrapper').addClass('how-it-works-on');
-			$('#jmpress').jmpress('init', {
-				
-				beforeChange: function(element) {
-				$('.hiw-nav a').removeClass('active');
-				$('.hiw-nav a').eq($(element).index()).addClass('active');
-				}
-				
-			});
-		} 
-				
-		return false;
-		
-	});
-	
-	$('body').on(event_type,'button#close-how-it-works', function(){
-		
-		var how_it_works_panel = $('#how-it-works');
-	
-		if ( $(how_it_works_panel).hasClass('fadeIn') ) {
-			$(how_it_works_panel).removeClass('fadeIn').addClass('fadeOut');
-			
-			$(how_it_works_panel).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-		
-			$(this).removeClass('animated fadeOut').addClass('hidden');	
-			$('.tlw-wrapper').removeClass('how-it-works-on');
-			$('#jmpress').jmpress('deinit');
-			});
-		} 
-				
-		return false;
-		
-	});
-	
-	//BUSINESS CAROUSEL FUNCTIONS
-	$('.carousel').carousel({
-	interval: 5000,
-	pause: "hover"
-	});
-	
-	$('#business-carousel').on('slide.bs.carousel', function() {	
-	$(this).next().find('.banner-item').removeClass('active');
-	});
-	
-	//GO TO PAGE TOP
-	$('body').on(event_type,'button#back-2-top', function(){
-	
-		$('html, body').animate({ scrollTop: 0 }, 500);
-
-		return false;
-		
-	});
-	
 	//Scroll to button
 	
 	$('body').on(event_type,'a.scroll-to', function(){
@@ -232,144 +82,11 @@
 		
 	});
 	
-	// VIEW RADIO FILES BUTTON 
-	
-	$('body').on(event_type,'a#call-2-action-radio', function(){
-	
-		if ( $('.audio-files').hasClass('closed') ) {
-			$('html, body').animate({scrollTop: ($("#radio-player").offset().top - 20)}, 500);	
-			$('.audio-files').removeClass('closed').addClass('open');
-			$(this).addClass('active');
-		} else {
-			$('.audio-files').removeClass('open').addClass('closed');
-			$('div.mejs-pause').find('button').trigger('click');
-			$('html, body').animate({ scrollTop: 0 }, 500);
-			$(this).removeClass('active');
-		}
-		
-		return false;
-		
-	});
-	
-	// CLOSE AUDIO FILES
-	
-	$('body').on(event_type,'button#close-audio-files', function(){
-	
-	$('html, body').animate({ scrollTop: 0 }, 500);
-	
-	if ( $('.audio-files').hasClass('open') ) {
-		$('.audio-files').removeClass('open').addClass('closed');
-		$('a#call-2-action-radio').removeClass('active');
-			
-		$('div.mejs-pause').find('button').trigger('click');
-	}
-
-	return false;
-		
-	});
-	
-	/* END OF AUDIO FILES FUNCTIONS */
-	
-	
-	/* USER SIDEBAR BUTTON */
-	
-	$('body').on(event_type,'button#user-btn', function(){
-	
-		if ( $(this).parent().hasClass('closed') ) {
-			$(this).parent().removeClass('closed').addClass('open');
-			$(this).find('i').removeClass('fa-user').addClass('fa-times');
-		} else {
-			$(this).parent().removeClass('open').addClass('closed');
-			$(this).find('i').removeClass('fa-times').addClass('fa-user');
-		}
-		
-		if ( $('#quick-links').hasClass('open') ) {
-			$('#quick-links').removeClass('open').addClass('closed');
-		}
-		
-		return false;
-		
-	});
-	
-	$('body').on(event_type,'li.page_item_has_children > a', function(){
-		
-		var pihc_parent = $(this).parent();
-		
-		$('li.page_item_has_children').not(pihc_parent).removeClass('view-children').addClass('hide-children');
-	
-		if ( $(pihc_parent).hasClass('hide-children') ) {
-			$(pihc_parent).removeClass('hide-children').addClass('view-children');
-		} else {
-			$(pihc_parent).removeClass('view-children').addClass('hide-children');
-
-		}
-		
-		return false;
-		
-	});
-	
-	
-	/* POP UP LINKS MENU BUTTONS 
-	Button to close the service menu links	
+	/* 	MAIN TOP NAVIGATION FUNCTIONS 
+		Button functions to open top navigation and 
+		control the internal menu functions as well
+		as close the menu.
 	*/
-	
-	$('body').on(event_type,'div.links-menu > button.close-btn', function(){
-		
-		var parent = $(this).parent();
-		
-		if ( $(parent).hasClass('open') ) {
-			$(parent).removeClass('open').addClass('closing');
-			
-			$('.closing').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-				$(this).toggleClass('closing closed');	
-				$('.tlw-wrapper').removeClass('links-open');
-			});
-			
-		}
-		
-		return false;
-		
-	});
-	
-	/*  SERVICE DROP-DOWN LINKS
-		This link will show and hide hiden links in the services pop-up links
-	 */
-	
-	$('body').on(event_type,'a.dropdown-link', function(){
-		
-		var dd_parent = $(this).parent();
-		
-		$('li.with-children').not(dd_parent).removeClass('open').addClass('closed');
-	
-		if ( $(dd_parent).hasClass('closed') ) {
-			$(dd_parent).removeClass('closed').addClass('open');
-		} else {
-			$(dd_parent).removeClass('open').addClass('closed');
-		}
-		
-		return false;
-		
-	});
-	
-	/*  SERVICE MENU BUTTON 
-		Button to activate the services pop-up menu
-	*/
-	
-	$('body').on(event_type,'button.service-menu-btn', function(){
-		
-		var links_menu = $('div.links-menu');
-		
-		if ( $(links_menu).hasClass('closed') ) {
-		$(links_menu).removeClass('closed').addClass('open');
-		$('.tlw-wrapper').addClass('links-open');
-		} 
-	
-		
-		return false;
-		
-	});
-	
-	/* 	MAIN TOP NAVIGATION FUNCTIONS */
 	
 	$('body').on(event_type,'button#nav-btn', function(){
 		
@@ -432,67 +149,10 @@
 
 	return false;	
 	});
-	
-	/* 	FAQ's */
-	$('body').on(event_type,'div.faq-question', function(){
 		
-		var parent = $(this).parent();
-		var siblings = $(parent).siblings();
-		
-		//console.log(siblings);
-		
-		if (siblings.hasClass('item-open')) {
-		siblings.removeClass('item-open').addClass('item-closed');	
-		}
-		
-		parent.toggleClass('item-closed item-open');
-		
-		return false;
-		
-	});
-	$('body').on(event_type,'div.faq-header', function(){
-		
-		var next = $(this).next();
-		
-		if ($('div.faq-header').not(this).hasClass('faqs-open')) {
-		$('div.faq-header').removeClass('faqs-open').addClass('faqs-closed');	
-		$('div.faqs-sub-pgs').removeClass('faqs-sub-open').addClass('faqs-sub-closed');	
-		}
-		
-		$(this).toggleClass('faqs-closed faqs-open');
-		next.toggleClass('faqs-sub-closed faqs-sub-open');
-		
-		return false;
-		
-	});
-	
-	/* DOWNLOAD BOOKLET GUIDE BUTTON */
-	
-	$('body').on(event_type,'button#file-download-btn', function(){
-		
-		var next = $(this).next();
-		
-		$(next).toggleClass('form-open form-closed');	
-		$('html, body').animate({scrollTop: ($("button#file-download-btn").offset().top) - 30}, 500);
-		
-		return false;
-		
-	});
-	
-	$(document).bind('gform_confirmation_loaded', function(event, formId){
-            if(formId === 19 && $('a#download-file-btn').length === 1) {
-               $('a#download-file-btn').removeClass('hidden');
-               $('html, body').animate({scrollTop: ($("button#file-download-btn").offset().top - 20)}, 500);	
-            }
-            
-            if(formId === 20 && $('#hidden-download').length === 1) {
-               $('#hidden-download').removeClass('hidden');
-               $('.gform_heading').addClass('hidden');
-               $('html, body').animate({scrollTop: ($("#hidden-download").offset().top - 20)}, 500);	
-            }
-    });
-	
-	/* 	HEADER SEARCH BUTTON */
+	/* 	HEADER SEARCH BUTTON 
+		Button functions for search pop up menu
+	*/
 	
 	$('body').on(event_type,'button#search-btn', function(){
 	
@@ -504,6 +164,7 @@
 		return false;
 		
 	});
+	
 	$('body').on(event_type,'button#close-search', function(){
 	
 		if ( $('#search-pop-up').hasClass('on') ) {
@@ -521,7 +182,9 @@
 		
 	});
 	
-	/* POP UP LOCATION MAP */
+	/* POP UP LOCATION MAP 
+	Button functions for location map pop up
+	*/
 	
 	$('body').on(event_type,'li.office-location-link a', function(){
 	
@@ -563,7 +226,11 @@
         height: '300px'
     });
     
-    //POST CONTINUE READING BUTTONS
+    /* POST CONTINUE READING BUTTONS
+	   Functions for the read more button which reveals more
+	   content it also has a close function as well. 
+    */
+    
 	$('body').on('click','button#continue-read-btn', function(){
 		
 		var inner_h = $('#content-extra-inner').outerHeight();
@@ -593,23 +260,7 @@
 		return false;
 		
 	});
-	
-	 /* SERVICES SECTION
-	View all links button  
-   */
-   
-   $('body').on('click','button#show-more-links-btn', function(){
-	   	   
-		var text = $(this).find('span').text();
-		
-		$(this).parent().toggleClass('view-all-links');
-		
-		$(this).find('span').toggleClass('sr-only').text((text === "View More") ? "Close" : "View More");
-		
-		return false;
-	});
-   
-    
+
      /* TOOLKIT SCROLLER 
 	Adds new styled scroll bars to media feeds   
    */
@@ -627,34 +278,6 @@
     /* ACCESSABILITY FUNCTIONS 
 	   Button actions to control the text size
     */
-    
-    $('body').on(event_type,'.type-post button.access-btn', function(){
-    
-    	var txt_size = $(this).attr('data-role');
-    	
-    	$(this).siblings().removeClass('active');
-    	$(this).addClass('active');
-    	
-    	 $('body').removeClass('txt-md txt-lg txt-sm').addClass(txt_size);
-    	 $.cookie('font_size', txt_size, { path: '/' } );  
-	     	     			
-		return false;
-		
-	});
-	
-	$('body').on(event_type,'.type-page button.access-btn', function(){
-    
-    	var txt_size = $(this).attr('data-role');
-    	
-    	$(this).siblings().removeClass('active');
-    	$(this).addClass('active');
-    	
-    	 $('body').removeClass('txt-md txt-lg txt-sm').addClass(txt_size);
-    	 $.cookie('font_size', txt_size, { path: '/' } );  
-	     	     			
-		return false;
-		
-	});
 	
 	$('body').on(event_type,'#txt-only-content button.access-btn', function(){
     
@@ -667,34 +290,10 @@
 	     	     			
 		return false;
 		
-	});
-	
-	
-		$('#feedback-carousel').carousel({
-			pause: false,
-			interval: 7000
-		});
+	});	
 		
-			if ($('#enqiry-start-form')) {
-			
-			$('#enqiry-start-form').show();	
-				
-			}
-			
-		var fa_fix = $('#cookie-law-info-bar').next();
-		
-		if ($(fa_fix).is('i')) {
-			
-			if ($(fa_fix).next().is('i')) {
-			$(fa_fix).next().remove();	
-			}
-			
-			$('#cookie-law-info-bar').next().remove();
-			
-		}
-		
-		/* XMAS Pop up Function
-	   This function controls the Xmas pop up box
+	/* XMAS Pop up Function
+	  This function controls the Xmas pop up box
     */
     
     	var xmasBox = function(){
@@ -740,6 +339,12 @@
 			return false;
 			
 		});
+		
+		
+		/* TOOLKIT SLIDER NAVIGATION 
+			Functions for the toolkit buttons to 
+			move to next slide.	
+		*/
 		
 		$('body').on(event_type, '.tk-slider-nav a', function(){
 			
@@ -868,18 +473,7 @@
 		startFeedbackInterval();
 		}
 		
-		/* QUICK LINKS IN BANNER SECTION */
-		
-		$('body').on(event_type, 'nav.banner-links a', function(){
-			
-			var hash = $(this).attr('href');
-    		var scrollTarget = $(hash).offset().top - 40;
-    		current_section = hash;
-    		 
-    		$('html, body').animate({ scrollTop: scrollTarget }, 500);	   
-
-			return false;
-		});
+		/* BANNER SECTION SCROLL DOWN BUTTON */
 		
 		$('body').on(event_type, 'button#scroll-down-btn', function(){
 			var next = $(this).parent().next().find('a.section-target');
@@ -1166,12 +760,7 @@
 	/* END DOC READY FUNCTION */
 	
 	$(window).load(function(){
-		
-/*
-		if ($('#services-dd').length === 1) {
-		$('#services-dd').selectpicker('refresh');
-		}
-*/
+
 		
 		if ($('a#call-2-action-radio').length === 1) {
 			$('#call-2-action-radio').removeAttr('disabled');
@@ -1202,7 +791,6 @@
 	$(window).scroll(function(){
 
 		var scroll = $(window).scrollTop();
-		var header_h = $('.header').outerHeight();
 		var h = $(window).height();
 		var section_targets = $('a.section-target');
 		var current_section_id;
@@ -1226,22 +814,6 @@
 				}	
 				
 			});
-		}
-	//console.log(current_section);	
-		
-		if ( scroll > Math.ceil(h/2) ) {
-		$('button#back-2-top').removeClass('hidden').addClass('visible fadeIn');	
-		}
-		
-		if ( scroll <= header_h && $('button#back-2-top').hasClass('visible') ) {
-		$('button#back-2-top').removeClass('fadeIn').addClass('fadeOut');	
-		
-			$('button#back-2-top').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-		
-			$(this).removeClass('visible fadeOut').addClass('hidden');	
-		
-			});
-			
 		}
 		
 	});
