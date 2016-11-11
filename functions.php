f9f0d9c6f9fd37ef8891fdbbb2709f62c9b6e80c
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-11-11 16:23:31 +0000

    Update jquery enqueue.

--- functions.php
+++ functions.php
@@ -14,9 +14,9 @@ function tlw_scripts() {
 	
 	// Load JS
 	//wp_enqueue_script( 'jquery' );
-	wp_deregister_script('jquery');
-    wp_enqueue_script('jquery-min', "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js", null, '3.1.1', true);
+	//wp_deregister_script('jquery');
 	//wp_enqueue_script( 'jquery-ui-core' );
+    wp_enqueue_script('jquery-min', "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js", null, '3.1.1', true);
 	wp_enqueue_script( 'jquery-cookie', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js', array('jquery'), '1.4.1', true );
 	wp_enqueue_script( 'slim-scroll', 'https://cdnjs.cloudflare.com/ajax/libs/jQuery-slimScroll/1.3.6/jquery.slimscroll.min.js', array('jquery'), '1.3.6', true );
 	wp_enqueue_script( 'bootstrap-select', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.7.3/js/bootstrap-select.min.js', array('jquery'), '1.0.0', true );
