60186d3d34a75ffd607eece2024cf5f4002706cc
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-11-17 10:36:02 +0000

    Update jobs title

--- _/functions/tlw_jobs_cpt.php
+++ _/functions/tlw_jobs_cpt.php
@@ -23,7 +23,7 @@ function register_cpt_tlw_vacancies_cpt() {
 		'labels' => $labels,
 		'hierarchical' => false,
 		'description' => 'Post type for TLW Job vacancies',
-		'supports' => array( 'editor' ),
+		'supports' => array( 'title', 'editor' ),
 		'public' => true,
 		'show_ui' => true,
 		'show_in_menu' => true,
@@ -46,8 +46,9 @@ function register_cpt_tlw_vacancies_cpt() {
 
 	register_post_type( 'tlw_vacancies_cpt', $args );
 	
-	remove_post_type_support('tlw_vacancies_cpt', 'title');
+	//remove_post_type_support('tlw_vacancies_cpt', 'title');
 	
+/*
 	add_action( 'edit_form_after_title', 'vacancy_title' );
 	
 	function vacancy_title() {
@@ -65,6 +66,7 @@ function register_cpt_tlw_vacancies_cpt() {
 		}
 		
 	}
+*/
 } 
 
 ?>
