0938dbc70b9e0c30a52fa37cd8bdc51b7eec4c68
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-01-17 16:19:48 +0000

    Fix async

--- functions.php
+++ functions.php
@@ -26,12 +26,13 @@ function tlw_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'tlw_scripts' );
 
-//if ($_SERVER['SERVER_NAME']=='www.tlwsolicitors.co.uk') {
+if ($_SERVER['SERVER_NAME']=='www.tlwsolicitors.co.uk') {
 function add_async_attribute($tag, $handle) {
 	//echo '<pre>';print_r($handle);echo '</pre>';
    
    // add script handles to the array below
    $scripts_to_defer = array(
+   'jquery',
    'modernizr-min', 
    'plupload'
    );
@@ -45,7 +46,7 @@ function add_async_attribute($tag, $handle) {
 }
 
 add_filter('script_loader_tag', 'add_async_attribute', 10, 2);
-//}
+}
 
 if ($_SERVER['SERVER_NAME']=='www.tlwsolicitors.co.uk') {
 	function ewp_remove_script_version( $src ){
