0fde2366b68651bba7d51fb1a72470ae1a537893
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-01-19 11:40:58 +0000

    async defer fief script.

--- footer.php
+++ footer.php
@@ -95,7 +95,7 @@
 	<?php include (STYLESHEETPATH . '/_/inc/xmas/pop-up.inc'); ?>
 	
 	
-	<script type="text/javascript" src="https://api.feefo.com/api/javascript/tlw-solicitors" async></script>
+	<script type="text/javascript" src="https://api.feefo.com/api/javascript/tlw-solicitors" async defer></script>
 	<div id="feefo-service-review-floating-widgetId"></div>
 	
 	<?php include (STYLESHEETPATH . '/_/inc/global/site-loader.inc'); ?>
