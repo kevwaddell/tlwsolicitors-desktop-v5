8040bfe28426f229eadedd17008586cc41c179d2
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-01-31 09:14:13 +0000

    Remove commented out javascript from footer.

--- footer.php
+++ footer.php
@@ -100,22 +100,6 @@
 	<div id="feefo-service-review-floating-widgetId"></div>
 	
 	<?php wp_footer(); ?>
-		
-<!--
-	<script>
-      var loadDeferredStyles = function() {
-        var addStylesNode = document.getElementById("deferred-styles");
-        var replacement = document.createElement("div");
-        replacement.innerHTML = addStylesNode.textContent;
-        document.body.appendChild(replacement);
-        addStylesNode.parentElement.removeChild(addStylesNode);
-        document.body.classList.remove("atfc-desktop-css");
-      };
-      var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
-      if (raf) { raf(function() { window.setTimeout(loadDeferredStyles, 0); });
-     } else { window.addEventListener('load', loadDeferredStyles); }
-    </script>
--->
     
 	</body>
 </html>
--- parts/footers/footer-landing-page.php
+++ parts/footers/footer-landing-page.php
@@ -1,21 +1,6 @@
 		
 		<?php include (STYLESHEETPATH . '/_/inc/global/no-script.inc'); ?>
 		
-<!--
-		<script>
-	      var loadDeferredStyles = function() {
-	        var addStylesNode = document.getElementById("deferred-styles");
-	        var replacement = document.createElement("div");
-	        replacement.innerHTML = addStylesNode.textContent;
-	        document.body.appendChild(replacement);
-	        addStylesNode.parentElement.removeChild(addStylesNode);
-	        document.body.classList.remove("atfc-desktop-css");
-	      };
-	      var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
-	      if (raf) { raf(function() { window.setTimeout(loadDeferredStyles, 0); });
-	     } else { window.addEventListener('load', loadDeferredStyles); }
-		 </script>
--->
     
 		<?php wp_footer(); ?>
 
