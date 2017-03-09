a2c96d0b6ebf89b7c1f8d3d13bf44c89609de03c
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-03-09 15:23:52 +0000

    Test dandelion

--- page.php
+++ page.php
@@ -14,7 +14,7 @@
 	?>	
 	
 	<!-- MAIN CONTENT START -->
-	<main id="main-content" class="main-content-section">
+	<main id="main-content">
 		
 		<!-- BANNER SECTION -->
 		<?php if ($banner_active) { 
